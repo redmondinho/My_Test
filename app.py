438bde7d0f3fca682a86cfe988932e99b93d34df
Tom Redmond <redmondinho@gmail.com>
2019-12-03 21:57:43 +0000

    Changes via GitKraken

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/page2')
+def page2():
+	return 'Page 2'
 
 
 if __name__ == '__main__':
